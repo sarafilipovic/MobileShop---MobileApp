243a7f1bc29bf3c0288598f05efb54617c0a979f
lukarebac97 <luka.rebac1@fpmoz.sum.ba>
2024-01-13 22:10:11 +0100

    Dodan account activity, dodane funkcionalnosti za side bar

---
+++ app/src/main/java/com/example/mobileshop_mobileapp/AccountActivity.java
@@ -0,0 +1,19 @@
+package com.example.mobileshop_mobileapp;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class AccountActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.account);
+
+
+    }
+}
--- app/src/main/java/com/example/mobileshop_mobileapp/HomeActivity.java
+++ app/src/main/java/com/example/mobileshop_mobileapp/HomeActivity.java
@@ -1,13 +1,62 @@
 package com.example.mobileshop_mobileapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class HomeActivity extends AppCompatActivity {
+    private DrawerLayout drawer;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                handleNavigationItemSelected(item);
+                return true;
+            }
+        });
+
+    }
+
+
+
+    public void handleNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        final int ACCOUNT_MENU_ID = R.id.account;
+        final int LOGOUT_MENU_ID = R.id.logout;
+
+        if (id == ACCOUNT_MENU_ID) {
+            Intent intent = new Intent(this, AccountActivity.class);
+            startActivity(intent);
+        }else if(id == LOGOUT_MENU_ID){
+            logoutUser();
+        }
+
+
+    }
+
+    public void openDrawer(View view) {
+        drawer.openDrawer(GravityCompat.START);
+    }
+
+    public void closeDrawer() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        }
     }
 }
