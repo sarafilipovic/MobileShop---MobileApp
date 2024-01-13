877f825a01628027e3655ba40cb9ed93c4965513
lukarebac97 <luka.rebac1@fpmoz.sum.ba>
2024-01-13 22:16:42 +0100

    implementiran funkcionalan side bar, dodana odjava

--- app/src/main/java/com/example/mobileshop_mobileapp/HomeActivity.java
+++ app/src/main/java/com/example/mobileshop_mobileapp/HomeActivity.java
@@ -9,13 +9,28 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import com.google.android.material.navigation.NavigationView;
 
 public class HomeActivity extends AppCompatActivity {
     private DrawerLayout drawer;
-
-
+    private FirebaseAuth auth = FirebaseAuth.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,7 +47,13 @@ public class HomeActivity extends AppCompatActivity {
 
     }
 
-
+    public void logoutUser(){
+        auth = FirebaseAuth.getInstance();
+        auth.signOut();
+        Toast.makeText(this, "Uspješno ste se odjavili", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 
     public void handleNavigationItemSelected(MenuItem item) {
         int id = item.getItemId();
