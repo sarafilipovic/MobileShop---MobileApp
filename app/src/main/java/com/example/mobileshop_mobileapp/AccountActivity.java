7327dff4dc0a31383f22f9c28df43778c344e691
lukarebac97 <luka.rebac1@fpmoz.sum.ba>
2024-01-13 22:21:59 +0100

    funkcionalonost dodana u account activity, dohvacannje imena i emaila iz baze

--- app/src/main/java/com/example/mobileshop_mobileapp/AccountActivity.java
+++ app/src/main/java/com/example/mobileshop_mobileapp/AccountActivity.java
@@ -1,19 +1,63 @@
 package com.example.mobileshop_mobileapp;
 
-import android.os.Bundle;
-import android.view.MenuItem;
-
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-import com.google.android.material.navigation.NavigationView;
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 public class AccountActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private DatabaseReference databaseReference;
+    private EditText novaLozinka;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.account);
 
+        auth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = auth.getCurrentUser();
+        TextView emailTextView = findViewById(R.id.userEmail);
+        TextView nameTextView = findViewById(R.id.userName);
 
+        if(currentUser != null){
+            String email = currentUser.getEmail();
+            emailTextView.setText(email);
+            databaseReference = FirebaseDatabase.getInstance().getReference().child("users").child(currentUser.getUid()).child("firstName");
+            databaseReference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    String firstName = snapshot.getValue().toString();
+                    nameTextView.setText(firstName);
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    Toast.makeText(AccountActivity.this, "Greska", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        }
     }
 }
